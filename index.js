f9fa9b91e562c4313adcc9d09a998984a48e89f3
Shahiduzzaman Tauhid <shahiduzzamantauhid@gmail.com>
2023-05-04 23:14:25 +0600

    added all

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const port = 3000;
 
 app.use(cors());
 
-
+app.get('/', (req, res) => {
+    res.send('Hello Vai This Is Food App!')
+})
+app.get('/chef', (req, res) => {
+    res.send(data)
+})
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = data.find(n => n.chef_id === id);
+    res.send(chef);
+})
 
 app.listen(port, () => console.log(`Foody BD listening on port ${port}!`))
