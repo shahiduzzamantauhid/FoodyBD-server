0a067b4ae0c8c476c9b7b786e81a24d94d131aae
Shahiduzzaman Tauhid <shahiduzzamantauhid@gmail.com>
2023-05-04 23:17:08 +0600

    added vercle

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello Vai This Is Food App!')
 })
+
 app.get('/chef', (req, res) => {
     res.send(data)
 })
+
 app.get('/chef/:id', (req, res) => {
     const id = req.params.id;
     const chef = data.find(n => n.chef_id === id);
